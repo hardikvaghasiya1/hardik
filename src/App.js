446e41e1dae36dd1f2658900cd626bad13ad4856
hardik <rnwlchardikvaghasiya@gmail.com>
2022-04-27 17:53:52 +0530

    array

--- src/App.js
+++ src/App.js
@@ -31,7 +31,58 @@ hardik();
 // b=20;
 // console.log(b);
 
+let arr1 = [100, 200, 300, 400, 500];
+let result1 = Array.isArray(arr1);
+console.log(result1);
 
+let arr2 = [100, 200, 300, 400, 500];
+let arr3 = [600, 700, 800, 900, 1000];
+let result2 = arr2.concat(arr3);
+console.log(result2);
+
+let arr4 = [100, 200, 300, 400, -500];
+let result3 = arr4.some(checknumber);
+console.log(result3);
+
+function checknumber(number){
+  return number > 0;
+}
+
+let arr5 = ["Hii", "Hellow", "Hey", "Hello",];
+let result4 = arr5.toString(arr5);
+console.log(result4);
+
+
+let arr6 = [100, 200, 300, 400, 500];
+arr6.shift();
+console.log(arr6);
+
+
+let arr7 = [100, 200, 300, 400, 500];
+arr7.unshift(600, 700);
+console.log(arr7);
+
+
+let arr8 = [100, 200, 300, 400, -500];
+let result5 = arr8.find(checknegative);
+console.log(result5);
+
+function checknegative(number){
+  return number < 0;
+}
+
+let arr9 = [100, 200, 300, 400, 500, 600];
+arr9.splice(2,3);
+console.log(arr9);
+
+
+let arr10 = [100, 200, 300, 400, 500, 600];
+arr10.push(700, 800);
+console.log(arr10);
+
+let arr11 = [100, 200, 300, 400, 500, 600];
+arr11.pop();
+console.log(arr11);
 
 
 
